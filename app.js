09e9ed9f2eb5cb17b1e4ce3ed9a7843a233d9fcc
Tonay <168505740+tonaySadhukhan@users.noreply.github.com>
2024-11-01 20:43:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const path=require('path');
+const http=require('http');
+const {Server}=require('socket.io');
+
+
+const express=require('express');
+const app=express();
+const server=http.createServer(app);
+
+const io=new Server(server);
+io.on("connection",(socket)=>{
+    console.log("al bal");
+    socket.on('data',(message)=>{
+        console.log(message);
+        io.emit('message',message);
+    });
+});
+
+app.get('/',(req,res)=>{
+    res.sendFile('C:\\Users\\Tonay Sadhukhan\\Desktop\\chat\\index.html');
+});
+
+server.listen(3000);
